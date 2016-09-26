1cb85f30d915f98bdfdce8b5ef4043c660d0fb46
gtechoogle <gtechoogle@gmail.com>
2016-09-26 23:29:47 +0800

    Fix bug on not show pics
    temp solution to show complete pictures, need to fix later

--- app/src/main/java/com/gtechoogle/wallpaper/bing/retrofit/BingPicApi.java
+++ app/src/main/java/com/gtechoogle/wallpaper/bing/retrofit/BingPicApi.java
@@ -4,6 +4,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface BingPicApi {
-    @GET("/HPImageArchive.aspx?format=js&idx=16&n=8")
+    @GET("/HPImageArchive.aspx?format=js&idx=23&n=8")
     Call<BingPic> getBingPic();
 }
